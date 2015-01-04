f3e3028b2df11d70e0e68ec234388c1f4e7214ce
Christophe TARDELLA <christophe.tardella@gmail.com>
2015-01-04 22:50:05 +0100

    Cluster OK -> a faire en TP

--- src/main/resources/static/js/greenestPlaceToLive.js
+++ src/main/resources/static/js/greenestPlaceToLive.js
@@ -1,16 +1,52 @@
+var selectedMarkers = [];
+var searchedProperties = ['essencefrancais', 'essence', 'localisation', 'nomvoie'];
+
 var map = L.map('map').setView([45.762179, 4.862226], 13);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
-        console.log('test');
+kebUtil.control.zoomInfo(map).addTo(map);
+var searchCtrl = L.control.fuseSearch({
+    maxResultLength : 10, 
+    showResultFct : function(feature, resultItem) {
+        var str = '<b>' + feature.properties[searchedProperties[0]] + '</b>';
+        for (var i = 1; i < searchedProperties.length; i++) {
+            str += '<br/>' + feature.properties[searchedProperties[i]];
+        }
+        resultItem.innerHTML = str;
+
+        resultItem.onclick = function() {
+            map.setView(kebUtil.toLatLng(feature.geometry.coordinates), 18);
+        }
+        return resultItem;
+    }
+});
+searchCtrl.addTo(map);
+
 $.ajax({
     url : '/data/abr_arbres_alignement.abrarbre_5000.geojson',
     dataType : 'json',
     success : function(data) {
+        searchCtrl.indexFeatures(data.features, searchedProperties);
+        
         var markers = new L.MarkerClusterGroup();
-        markers.addLayer(L.geoJson(data));
+        var geoJsonData = L.geoJson(data, {
+            pointToLayer : function(featureData, latlng) {
+                var marker = L.StatefulMarker(latlng);
+                marker.on('click', function(evt) {
+                    var targetMarker = evt.target;
+                    if(targetMarker.getState() === 'default') {
+                        targetMarker.setState('selected');
+                        selectedMarkers.push(targetMarker);
+                    } else if(targetMarker.getState() === 'selected') {
+                        targetMarker.setState('default');
+                        _.remove(selectedMarkers, targetMarker);
+                    }
+                });
+                return marker;
+            }
+        });
+        markers.addLayer(geoJsonData);
         map.addLayer(markers);
     },
     error : function() {
@@ -18,11 +54,9 @@ $.ajax({
     }
 });
 
-var purpleIcon = L.icon({
-    iconUrl: '/images/marker-icon-purple.png'
-});
-
-var changingMarker = L.marker([45.762186, 4.862247]).addTo(map);
-changingMarker.on('click', function() {
-    changingMarker.setIcon(purpleIcon);
-});
+function markAsTreated() {
+    _.each(selectedMarkers, function(marker) {
+        marker.setState('treated');
+    });
+    selectedMarkers = [];
+}
--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -23,5 +23,46 @@ var kebUtil = {
             
             return zoomInfoControl;
         }
+    }, 
+    
+    statefulMarker : function(coordinates, options) {
+        var marker = L.Marker.extend({
+            initialize: function (coordinates, options) {
+                L.Marker.prototype.initialize.call(this, coordinates); 
+                           
+                this._iconByState = options.states;                
+                this.setState('default');
+            },
+            
+            setState : function(state) {
+                var stateIcon = this._iconByState[state];
+                
+                if(stateIcon) {
+                    this.setIcon(stateIcon);                
+                    this._state = state;
+                } else {
+                    throw Error('No icon found for state \'' + state + '\'');
+                }
+            },
+            
+            getState : function() {
+                return this._state;
+            }
+        });
+        
+        return new marker(coordinates, options);
     }
+};
+
+var defaultStates = {
+    default : L.icon({iconUrl: '/images/marker-icon-green.png'}),
+    selected : L.icon({iconUrl: '/images/marker-icon-orange.png'}),
+    treated : L.icon({iconUrl: '/images/marker-icon-purple.png'}),
+};
+
+L.StatefulMarker = function (coordinates, options) {
+    var usedOptions = options || {};
+    usedOptions.states = usedOptions.states || defaultStates;
+    
+    return kebUtil.statefulMarker(coordinates, usedOptions);
 };

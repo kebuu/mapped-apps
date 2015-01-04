e0f063bc7fce8c5c1543679d4d07244692e4b12e
Christophe TARDELLA <christophe.tardella@gmail.com>
2015-01-04 23:42:51 +0100

    Export OK -> TP a faire

--- src/main/resources/static/js/export.js
+++ src/main/resources/static/js/export.js
@@ -1,22 +1,12 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        })
-    ],
-    view: new ol.View({
-        center: ol.proj.transform([6.126019, 45.043144], 'EPSG:4326', 'EPSG:3857'),
-        zoom: 16
-    })
-});
+var map = L.map('map').setView([48.781042, 2.057863], 17);
+
+var osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+L.marker([48.781042, 2.057863]).addTo(map).bindPopup('Chez CTA');
 
 var exportPNGElement = document.getElementById('export-png');
-
-exportPNGElement.addEventListener('click', function(e) {
-    map.once('postcompose', function(event) {
-        var canvas = event.context.canvas;
+var exportFn = function() {
+    leafletImage(map, function(err, canvas) {
         exportPNGElement.href = canvas.toDataURL('image/png');
+        exportPNGElement.click();
     });
-    map.renderSync();
-}, false);
+};

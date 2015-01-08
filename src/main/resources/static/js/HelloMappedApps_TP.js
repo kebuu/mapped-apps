45d89e975c0dc814a3edb35d7cb7838c6cdadc94
christophe tardella <christophe.tardella@dict.fr>
2015-01-08 13:47:23 +0100

    TP hellomappedapps OK

--- src/main/resources/static/js/HelloMappedApps_TP.js
+++ src/main/resources/static/js/HelloMappedApps_TP.js
@@ -4,7 +4,7 @@ var zenikaParis = [48.878933, 2.328639];
 
 
 /* STEP 1 : - Ajouter une carte -> un div#map a été prévu pour accueillir la carte
-            - Ajouter un fond de plan tuilé (tile) avec "l'attribution" : Zenika - Technozaure -> url de OpenStreetMap : 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            - Ajouter un fond de plan tuilé (tile) avec l'option "attribution" : Zenika - Technozaure | OSM -> url de OpenStreetMap : 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             - Centrer la carte sur la France avec un niveau de zoom de 5
             - Ajouter une "attribution"
             - doc map: http://leafletjs.com/reference.html#map-class
@@ -13,7 +13,7 @@ var zenikaParis = [48.878933, 2.328639];
 var map = L.map('map').setView(franceCenter, 5);
 
 var osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: 'Zenika - Technozaure'
+    attribution: 'Zenika - Technozaure | OSM'
 }).addTo(map);
 
 /* STEP 2 : - Ajouter une marker sur zenika Lyon avec une popup contenant le texte 'Zenika Lyon'
@@ -34,13 +34,39 @@ var osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             - Implementer la fonction zoomerSurLyon en centrant la carte sur Lyon avec un zoom de 14
 */
 
-//var osmMini = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-//
-//var mapbox = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-//    attribution: 'Zenika - Technozaure | Mapbox',
-//    id: 'ctardella.kko459nh'
-//});
-//
+/* STEP 4 : - Ajouter le control "Scale" en bas à droite de la carte:
+                -> doc http://leafletjs.com/reference.html#control-scale
+            - Ajouter le control "MousePosition" à la carte:
+                -> doc https://github.com/ardhi/Leaflet.MousePosition#leafletmouseposition
+                -> Penser à créer un layer spécifique pour le control
+            - Ajouter le control "MiniMap" à la carte en utilisant en fond de plan:
+                -> doc https://github.com/Norkart/Leaflet-MiniMap#leafletminimap
+            - Ajouter le control "ZoomInfo" à la carte:
+                -> Il s'agit d'un control que j'ai réalisé : kebUtil.control.zoomInfo(map).addTo(map);
+*/
+
+/* STEP 5 : - Créer un tileLayer de fond de plan en utilisant mapbox sans l'ajouter à la carte
+                -> url : 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png'
+                -> option "attribution" : Zenika - Technozaure | MapBox
+                -> option "id" : ctardella.kko459nh
+            - Ajouter le control "Layers" à la carte
+                -> Utiliser les couches OpenStreetMap et MapBox comme "baseLayers" et les couches velov et poubelle comme "overlays"
+                -> doc http://leafletjs.com/reference.html#control-layers
+*/
+
+/* STEP M&M : - Quelle est la valeur de l'échelle en km au niveau de zoom 9 ?
+*/
+
+/* STEP Bonus : - Regarder la doc des différents controls pour adapter changer leur comportement
+*/
+
+var osmMini = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+
+var mapbox = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+    attribution: 'Zenika - Technozaure | Mapbox',
+    id: 'ctardella.kko459nh'
+});
+
 var velov = L.tileLayer.wms("https://download.data.grandlyon.com/wms/smartdata", {
     layers: 'jcd_jcdecaux.jcdvelov',
     format: 'image/png',
@@ -60,23 +86,23 @@ var zenIcon = L.icon({
 
 L.marker(zenikaLyon).addTo(map).bindPopup('Zenika Lyon');
 L.marker(zenikaParis, {icon: zenIcon}).addTo(map).bindPopup('Zenika Paris');
-//
-//var baseMaps = {
-//    "MapBox": mapbox,
-//    "OpenStreetMap": osm
-//};
-//
-//var overlayMaps = {
-//    "Velov": velov,
-//    "Corbeille": garbage
-//};
-//
-//L.control.layers(baseMaps, overlayMaps).addTo(map);
-//L.control.scale({position : 'bottomright'}).addTo(map);
-//L.control.mousePosition().addTo(map);
-//new L.Control.MiniMap(osmMini).addTo(map);
-//kebUtil.control.zoomInfo(map).addTo(map);
-//
+
+var baseMaps = {
+    "MapBox": mapbox,
+    "OpenStreetMap": osm
+};
+
+var overlayMaps = {
+    "Velov": velov,
+    "Corbeille": garbage
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+L.control.scale({position : 'bottomright'}).addTo(map);
+L.control.mousePosition().addTo(map);
+new L.Control.MiniMap(osmMini).addTo(map);
+kebUtil.control.zoomInfo(map).addTo(map);
+
 function zoomerSurLyon() {
     console.warn('Not implemented yet');
     map.setView(zenikaLyon, 14);

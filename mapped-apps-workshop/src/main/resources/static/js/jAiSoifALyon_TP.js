b5eb8e5952fd09273a6f7ac967b9028bb6bdf8db
Christophe TARDELLA <christophe.tardella@gmail.com>
2015-04-22 22:30:36 +0200

    Revue TP 2

--- mapped-apps-workshop/src/main/resources/static/js/jAiSoifALyon_TP.js
+++ mapped-apps-workshop/src/main/resources/static/js/jAiSoifALyon_TP.js
@@ -5,20 +5,24 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 */
 
 /* STEP 1 : - Ajouter les couches WMS de fontaines de la ville de Lyon :
-                -> url de la couche de velov : https://download.data.grandlyon.com/wms/grandlyon
-                -> nom de la couche de velov : epo_eau_potable.epobornefont
+                -> url de la couche de fontaine : https://download.data.grandlyon.com/wms/grandlyon
+                -> nom de la couche de fontaine : epo_eau_potable.epobornefont
+                - doc : http://leafletjs.com/reference.html#tilelayer-wms
 */
 
 /* STEP 2 : - Activer la geolocalisation :
-                - Compléter la fonction locateMe en demandant une geolocalisation
+                - Compléter la fonction locateMe (cf. bas du fichier) en demandant une geolocalisation
+                    -> docs http://leafletjs.com/reference.html#map-locate
                 - Ajouter un eventHandler sur la carte afin de réagir à l'évènement "locationfound"
-                - Centrer sur la position trouvée et y ajouter un marker avec une popup indiquant "Votre position"
-                - docs  http://leafletjs.com/reference.html#map-locate
-                        http://leafletjs.com/reference.html#map-locationfound
+                    -> docs http://leafletjs.com/reference.html#events
+                            http://leafletjs.com/reference.html#location-event
+                - Centrer sur la position trouvée et y ajouter un marker avec une popup ouverte indiquant "Votre position" (cf. TP précédant)
+                    -> doc  http://leafletjs.com/reference.html#map-locationfound
+                            http://leafletjs.com/reference.html#map-openpopup
 */
 
 /* STEP 3 : - Trouver la fontaine la plus proche :
-                - Compléter la fonction findClosestFountain en utilisant la fonction kebUtil.findClosestFountain
+                - Compléter la fonction findClosestFountain (cf. bas du fichier) en utilisant la fonction kebUtil.findClosestFountain
                     -> il suffit de lui passer en paramètre l'objet LatLng correspondant à votre position
                 - Regarder le code de la fonction kebUtil.findClosestFountain et vérifier qu'on comprend ce qu'il fait
                 - doc   http://docs.geoserver.org/stable/en/user/services/wfs/reference.html
@@ -27,17 +31,19 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
 /* STEP 4 : - Lancer une requête de type GetFeatureInfo et afficher le résultat sur la carte :
                 - Ajouter un évènement au 'click' sur la carte
-                - Appeler alors la méthode kebUtil.getFeatureInfo ayant pour paramètres : la carte, un objet json permettant de configurer la requête GetFeatureInfo et défini ainsi:
+                    -> doc  http://leafletjs.com/reference.html#events
+                            http://leafletjs.com/reference.html#event-objects
+                - Appeler alors la méthode kebUtil.getFeatureInfo ayant pour paramètres : la carte et un objet json (permettant de configurer la requête GetFeatureInfo) défini ainsi:
                     -> {
-                            x : null, // Position x du point cliqué dans le contexte du conteneur : http://leafletjs.com/reference.html#map-containerpointtolayerpoint
-                            y : null, // Position y du point cliqué dans le contexte du conteneur : http://leafletjs.com/reference.html#map-containerpointtolayerpoint
-                            bbox : null, // BBox de la carte en String : http://leafletjs.com/reference.html#latlngbounds-tobboxstring
+                            x : null, // Position x du point cliqué dans le contexte du conteneur : http://leafletjs.com/reference.html#events-containerpoint
+                            y : null, // Position y du point cliqué dans le contexte du conteneur : http://leafletjs.com/reference.html#events-containerpoint
+                            bbox : null, // BBox de la carte en String : http://leafletjs.com/reference.html#map-getbounds http://leafletjs.com/reference.html#latlngbounds-tobboxstring
                             width : null, // Longueur de la carte : http://leafletjs.com/reference.html#map-getsize
-                            height : null, // Hauteur de la carte : http://leafletjs.com/reference.html#map-getsize
+                            height : null // Hauteur de la carte : http://leafletjs.com/reference.html#map-getsize
                     }
 */
 
-/* STEP M&M's : - Quelle est le nom de la fontaine la plus proche ?
+/* STEP M&M's : - Quelle est le nom de la fontaine la plus proche (il faut zoomer dessus et cliquer dessus sans cliquer sur le marqueur)?
 */
 
 /* STEP Bonus : - Regarder le code de la fonction kebUtil.getFeatureInfo et vérifier qu'on comprend ce qu'il fait

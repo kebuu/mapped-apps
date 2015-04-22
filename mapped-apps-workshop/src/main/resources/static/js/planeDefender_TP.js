9586c15567998d3fc8ab24beeaa832415916ddf8
Christophe TARDELLA <christophe.tardella@gmail.com>
2015-04-22 23:45:41 +0200

    Revue TP 4

--- mapped-apps-workshop/src/main/resources/static/js/planeDefender_TP.js
+++ mapped-apps-workshop/src/main/resources/static/js/planeDefender_TP.js
@@ -9,18 +9,19 @@ var defences = [];
                 -> doc : https://github.com/Leaflet/Leaflet.draw#controldraw
  */
 
-/* STEP 2 : - Dessiner des lignes et les ajouter à la carte
+/* STEP 2 : - Dessiner des lignes et les ajouter à la carte en interceptant le bon événement
                 -> doc : https://github.com/Leaflet/Leaflet.draw#drawcreated
  */
 
 /* STEP 3 : - Defendez votre avion
                 -> Ajouter les lignes dessinées au tableau de défence (definie ci-dessus) en les convertissant
                     préalabelement en GeoJson
-                -> doc : http://leafletjs.com/reference.html#polyline-togeojson
+                -> doc : http://leafletjs.com/reference.html#polyline-togeojson (Un objet polyline est un layer leaflet)
             - Bloquer les missiles en dessinant des défences
  */
 
 /* STEP M&M's : - Quel est la distance parcouru pas l'avion lorsqu'il ne se fait pas exploser (à la centaine de mètre près) ?
+                    -> La longueur les lignes dessinées est indiquée lors du tracé
  */
 
 /* STEP Bonus : - Ajouter la fonctionnalité que les défences placées sur le trajet de l'avion font exploser l'avion
